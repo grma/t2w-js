37e20630206bbaa91b70bd8bc47a489410df843a
Joey Lee <jayd005@gmail.com>
2015-02-22 02:02:15 +1100

    not sure why its cutting off

--- Quotes.js
+++ Quotes.js
@@ -2,11 +2,19 @@ var response;
 var req = new XMLHttpRequest();
 var url = "http://iheartquotes.com/api/v1/random?format=json&source=oneliners+riddles+misc";
 
-req.open('GET', url, false);
+req.open('GET', url, true);
 req.send();
 
-response = JSON.parse(req.responseText);
-if(response) {
-   var msg = response.quote;
-   Talk2Watch.sendSms(msg, "Quotes");
-}
+req.onload = function(e) {
+
+   if (req.readyState == 4) {
+      if (req.status == 200) {
+         response = JSON.parse(req.responseText);
+         if (response) {
+            var msg = response.quote;
+            Talk2Watch.sendSms(msg, "Quotes");
+         }
+      }
+   }
+};
+req.send(null);
