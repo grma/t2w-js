e0e89cfe08a4c011740733678ebe8843b56c6592
Joey Lee <jayd005@gmail.com>
2015-02-22 11:04:49 +1100

    test t2w json

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,17 @@
-Talk2Watch.sendSms("Hello World!", "Testing");
+var response;
+var req = new XMLHttpRequest();
+var url = "http://yeoji.com/t2w/test";
+
+req.open('GET', url, true);
+req.onload = function(e) {
+
+    if (req.readyState == 4) {
+        if (req.status == 200) {
+            response = JSON.parse(req.responseText);
+            if (response) {
+                Talk2Watch.sendSms(response.message, "Testing");
+            }
+        }
+    }
+};
+req.send(null);
